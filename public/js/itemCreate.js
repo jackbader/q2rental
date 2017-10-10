4be40e55d2401b58202811c2b3cac2151e9fd94a
Brodie Balser <balserbrodie@gmail.com>
2017-10-10 15:55:40 -0600

    post works now

--- public/js/itemCreate.js
+++ public/js/itemCreate.js
@@ -2,36 +2,47 @@ $(document).ready(() => {
 
   $("form").on('submit', (e) => {
     e.preventDefault()
-    const title = $('#title').val()
-    const desc = $('#desc').val()
-    const daily_price = $('#daily_price').val()
-    const cat = $('#cat').val()
-    const img = $('#img').val()
 
     $.getJSON('/token')
       .done((data) => {
-        console.log(data)
+        const user_id = data.cookies.userId
+        const title = $('#title').val()
+        const desc = $('#desc').val()
+        const daily_price = $('#daily_price').val()
+        const cat = $('#cat').val()
+        const img = $('#img').val()
+
+        const item = {
+          title: title,
+          desc: desc,
+          img_url: img,
+          cat: cat,
+          owner_id: user_id,
+          rented: false,
+          daily_price: daily_price
+        }
+
+        post(item)
+
       })
       .fail(($xhr) => {
         Materialize.toast($xhr.responseText, 3000);
       })
     })
 
-  // const item = {
-  //   title: title,
-  //   desc: desc,
-  //   img_url: img,
-  //   cat: cat,
-  //   owner_id: user_id,
-  //   rented: false,
-  //   daily_price: daily_price
-  // }
+  function post(item) {
 
-  // $.ajax(item)
-  //   .done(() => {
-  //     // window.location.href = '/index.html';
-  //   })
-  //   .fail(($xhr) => {
-  //     Materialize.toast($xhr.responseText, 3000);
-  //   })
+    const jaxObj = {
+      method: "POST",
+      url: '/items',
+      data: item,
+    }
+    $.ajax(jaxObj)
+      .done(() => {
+        console.log('got em')
+      })
+      .fail(($xhr) => {
+        Materialize.toast($xhr.responseText, 3000);
+      })
+    }
 })
