02717ba56386e49be94fc48f3049a4f86a99d18a
Dalton Winter <blinkin1808@gmail.com>
2017-10-11 14:27:45 -0600

    renting on profile works

--- public/js/profile.js
+++ public/js/profile.js
@@ -2,6 +2,10 @@ $(document).ready(() => {
 
   console.log('profile js linked!')
 
+  // $('#itemList').click(function(event) {
+  //   console.log(event.target)
+  // })
+
 
     $.getJSON('/token')
       .done((data) => {
@@ -22,11 +26,66 @@ $(document).ready(() => {
             $('#username').text('Username: ' + username)
             const join = user[0].created_at
             const date = new Date(join)
-            $('#joindate').text("joindate: " + (date.getMonth() +1) + '/' + new Date(join).getDate() + '/' + date.getFullYear())  
-            //new Date(join)
+            $('#joindate').text("joindate: " + (date.getMonth() +1) + '/' + new Date(join).getDate() + '/' + date.getFullYear())
+
           })
           .fail(($xhr) => {
             Materialize.toast($xhr.responseText, 3000);
           })
         })
       })
+
+      $.getJSON('/token')
+        .done((data) => {
+          console.log(data)
+          const user_id = data.cookies.userId
+          console.log("we made it")
+
+          const jaxObj = {
+            method: "GET",
+            url: `/items`
+          }
+          $.ajax(jaxObj)
+            .done((items) => {
+              console.log(items);
+              for(const item of items){
+                if (item.owner_id === user_id) {
+
+
+
+                  const $card = $('<div>').addClass('card card-image');
+
+                  const $cardContent = $('<div>').addClass('card-content black-text')
+                  const $span = $('<span>').addClass('card-title')
+                  $span.text(item.title)
+                  const $pPrice = $('<p>')
+                  $pPrice.text("$" + item.daily_price + " a day.")
+                  const $p = $('<p>')
+                  $p.text(item.desc)
+                  const $img = $('<img>').attr({ src: item.img_url, alt: item.title, height: 80, width: 80 });
+
+
+                  $card.append($img);
+                  $cardContent.append($span)
+                  $cardContent.append($pPrice)
+                  $cardContent.append($p)
+                  $card.append($cardContent)
+                  $card.click(function() {
+                    window.location.href = `/item.html?id=${item.id}`
+                  })
+                  let li = $('<li>')
+                  li.append($card)
+                  $('#itemList').append(li);
+
+
+
+                  console.log(item)
+                }
+              }
+
+
+            })
+            .fail(($xhr) => {
+              Materialize.toast($xhr.responseText, 3000);
+            })
+          })
